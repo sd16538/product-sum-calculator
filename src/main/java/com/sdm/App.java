7ad6b0f223bd918e43b1cb917d0cfdcae245d1b6
sd16538 <shuchita12@gmail.com>
2016-07-21 18:33:14 +1000

    added stub class and test

--- src/main/java/com/sdm/App.java
+++ src/main/java/com/sdm/App.java
@@ -1,13 +1,11 @@
 package com.sdm;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.concurrent.atomic.AtomicLong;
+
+class App {
+    private final AtomicLong counter = new AtomicLong();
+
+    long badlyNamedMethod() {
+        return counter.incrementAndGet();
     }
 }
